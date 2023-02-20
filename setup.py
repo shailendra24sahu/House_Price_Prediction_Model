4b46518f4a1cebe32047965aa7149d17acf3d146
shailendra24sahu <sahushailendrakumar053@gmail.com>
2023-02-20 17:16:22 +0530

    created exception and  logger

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ def get_requirements_list() -> List[str]:
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
-        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
-        if HYPHEN_E_DOT in requirement_list:
-            requirement_list.remove(HYPHEN_E_DOT)
+        # requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        # if HYPHEN_E_DOT in requirement_list:
+        #     requirement_list.remove(HYPHEN_E_DOT)
         return requirement_list
 
 
@@ -36,3 +36,4 @@ description=DESRCIPTION,
 packages=find_packages(), 
 install_requires=get_requirements_list()
 )
+
